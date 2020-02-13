39048a37c0a3217ae957ad2b97723c1f7d0c4142
john ruble <jrockwv@gmail.com>
2020-02-12 21:49:20 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
+
 
 const App = () => (
   <div>
     <Navigation />
+    <route path="/"  exact component={Home} />
+    <route path="/about"  exact component={About} />
+    <route path="/contact"  exact component={Contact} />
   </div>
 );
 
